9913408a39c77a58651541b8bbc8ab0a1eb74b25
shravya-34 <shettyshravya34@gmail.com>
2023-09-30 17:48:46 +0530

    basename added

--- askscribe/src/main.jsx
+++ askscribe/src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  <BrowserRouter>
+  <BrowserRouter basename='AskScribe'>
     <App />
   </BrowserRouter>,
 )
